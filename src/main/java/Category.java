fb45ba400117f1e7d724e0292e1cd04da42c7d9c
Anandita Melisa H <dita.lisaira@gmail.com>
2019-03-04 21:44:40 +0700

    Initial commit

---
+++ src/main/java/Category.java
@@ -0,0 +1,31 @@
+public class Category {
+    private String categoryID;
+    private String categoryName;
+
+    public Category(String categoryID, String categoryName) {
+        this.categoryID = categoryID;
+        this.categoryName = categoryName;
+    }
+
+    public void addNewCategory(){
+
+    }
+    public void deleteCategory(String categoryID){
+
+    }
+    public String getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(String categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
